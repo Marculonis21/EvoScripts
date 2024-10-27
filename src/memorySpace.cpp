76314793412af00bfd59d6aef2884a809816c2f8
Marculonis21 <markbecvar@gmail.com>
2024-10-27 14:24:15 +0100

    First draft WIP of template finding+matching in 1D memory

--- src/memorySpace.cpp
+++ src/memorySpace.cpp
@@ -1,11 +1,105 @@
 #include "memorySpace.hpp"
+#include <algorithm>
 #include <cstdint>
+#include <utility>
+#include <vector>
 
-Basic1D::Basic1D(uint64_t size) { memory = new uint8_t[size]; }
+Basic1D::Basic1D(uint64_t size) { memory = std::vector<uint8_t>(size); }
 
 uint8_t Basic1D::fetch(uint64_t address) const { 
-    return 5; 
+    return memory[address];
 }
 uint64_t Basic1D::allocate(uint64_t startAddress, uint64_t size) const {
-    return 5;
+    return 0;
+}
+
+templateInfo BaseMemoryType::loadInTemplate(uint64_t address) const {
+    uint64_t res = 0;
+    uint8_t offset = 0;
+
+    while (true) {
+        offset++;
+        switch (fetch(address+offset)) {
+            case 0x01:                      // nop0 
+                break;
+            case 0x02: res |= 1 << offset;  // nop1
+                break;
+            default:   return templateInfo{res, --offset};
+        }
+    }
+}
+
+uint64_t BaseMemoryType::matchTemplate(uint64_t address, templateInfo pattern) const {
+    // TODO: should be refactored and TESTED!
+
+    // vector of hit pairs - start address, distance from start
+    std::vector<std::pair<uint64_t, int>> hitVector;
+
+    const int searchSize = 5;
+
+    // search backward
+    uint64_t startPoint = std::max(address - searchSize,(uint64_t)0);
+
+    uint8_t offset = 0;
+    uint64_t check = 0;
+
+    uint64_t test = 0;
+
+    uint8_t instr;
+     
+    for (int i = startPoint; i < address; ++i) {
+        instr = fetch(i);
+
+        // section of continuous nops
+        if (instr == 0x01 || instr == 0x02) {
+            check |= (instr == 0x02 ? 1 : 0) << offset;
+            offset++;
+        }
+        else {
+            if (offset >= pattern.patternSize){
+                test = pattern.pattern ^ check;
+                if (((test + 1) & test) == 0) {
+                    // matching pattern found! -- save first pos of template, distance from starting address
+                    hitVector.emplace_back(i - offset, address - (i - offset));
+                }
+            }
+
+            check = 0;
+            offset = 0;
+        }
+    }
+
+    // forward
+    startPoint = address + pattern.patternSize + 1;
+
+    for (int i = startPoint; i < startPoint+searchSize; ++i) {
+        instr = fetch(i);
+
+        // section of continuous nops
+        if (instr == 0x01 || instr == 0x02) {
+            check |= (instr == 0x02 ? 1 : 0) << offset;
+            offset++;
+        }
+        else {
+            if (offset >= pattern.patternSize){
+                test = pattern.pattern ^ check;
+                if (((test + 1) & test) == 0) {
+                    // matching pattern found! -- save first pos of template, distance from starting address
+                    hitVector.emplace_back(i - offset, address - (i - offset));
+                }
+            }
+
+            check = 0;
+            offset = 0;
+        }
+    }
+
+    // choose some of the hit templates
+    
+    // TODO: some sort of exception?
+    if (hitVector.size() > 0) { return -1; }
+
+    // TODO: CHECK THIS
+    std::random_shuffle(hitVector.begin(), hitVector.end());
+    return hitVector[0].first;
 }
--- src/memorySpace.hpp
+++ src/memorySpace.hpp
@@ -2,21 +2,31 @@
 #define MEMORYSPACE_HPP
 
 #include <cstdint>
+#include <vector>
+
+struct templateInfo {
+  uint64_t pattern;
+  uint8_t patternSize;
+};
 
 class BaseMemoryType {
   public:
     virtual uint8_t fetch(uint64_t address) const = 0;
     virtual uint64_t allocate(uint64_t startAddress, uint64_t size) const = 0;
+
+    virtual templateInfo loadInTemplate(uint64_t address) const;
+    virtual uint64_t matchTemplate(uint64_t address, templateInfo pattern) const;
+
+  protected:
+    std::vector<uint8_t> memory;
 };
+
 
 class Basic1D : public BaseMemoryType {
   public:
     Basic1D(uint64_t size);
     uint8_t fetch(uint64_t address) const override;
     uint64_t allocate(uint64_t startAddress, uint64_t address) const override;
-
-  private:
-    uint8_t *memory;
 };
 
 #endif
