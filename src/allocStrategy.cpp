bee17a95cf00f31785d2d5d5d06f90cef869334f
Marculonis21 <markbecvar@gmail.com>
2024-11-14 18:10:12 +0100

    allocation strategy fixes, incorrect order of operands

--- src/allocStrategy.cpp
+++ src/allocStrategy.cpp
@@ -3,18 +3,23 @@
 #include "memoryHelperStructs.hpp"
 #include <cstdint>
 #include <optional>
+#include <iostream>
 
 // unnamed namespace for helper func
 namespace {
-bool fitsBetween(uint64_t size, const MemorySpace &m1, const MemorySpace m2) {
+bool fitsBetween(uint64_t size, const MemorySpace &m1, const MemorySpace &m2) {
 	return (m2.start - (m1.start + m1.size)) >= size;
-};
+}
 } // namespace
 
 std::optional<MemorySpace> AllocFirstFit::allocate(const AllocSpacesContainer &allocatedSpaces,
 												uint64_t caller, uint64_t size) const {
 
-	int middleIndex = allocatedSpaces.findInsertIndex(MemorySpace{caller, 0});
+	std::cout << "ALLOC FIRST FIT PROCEDURE" << std::endl;
+	// middle = insert index - 1 ... find index of current caller cell (find
+	// returns next index, so -1)
+	int middleIndex = allocatedSpaces.findInsertIndex(MemorySpace{caller, 0})-1;
+	std::cout << "middle index: " << middleIndex << std::endl;
 
 	bool backEnd, frontEnd = false;
 	int backIndex, frontIndex;
@@ -32,7 +37,8 @@ std::optional<MemorySpace> AllocFirstFit::allocate(const AllocSpacesContainer &a
 			MemorySpace back = allocatedSpaces.at(backIndex);
 			MemorySpace next = allocatedSpaces.at(backIndex + 1);
 
-			if (fitsBetween(size, back, next)) {
+			if (fitsBetween(size, next, back)) {
+				std::cout << "FITS FIRST" << std::endl;
 				// size of the new allocated space away from the 'next' block start
 				return MemorySpace{next.start - size, size};
 			}
@@ -43,9 +49,10 @@ std::optional<MemorySpace> AllocFirstFit::allocate(const AllocSpacesContainer &a
 
 		if (frontIndex < allocatedSpaces.size()) {
 			MemorySpace forward = allocatedSpaces.at(frontIndex);
-			MemorySpace prev = allocatedSpaces.at(frontIndex - 1);
+			MemorySpace prev    = allocatedSpaces.at(frontIndex - 1);
 
 			if (fitsBetween(size, prev, forward)) {
+				std::cout << "FITS END" << std::endl;
 				// starts right after the end of prev space
 				return MemorySpace{prev.start+prev.size, size};
 			}
