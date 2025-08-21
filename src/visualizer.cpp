0af98a87a2339c6982211533620eb9b076a6fcb5
Marculonis21 <markbecvar@gmail.com>
2025-08-21 10:25:14 +0200

    update to visualizer - printing to file

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -1,10 +1,16 @@
 #include "visualizer.hpp"
 #include "lpu.hpp"
+#include "lpu_addons.hpp"
+#include "lpu_pool.hpp"
+#include "memoryHelperStructs.hpp"
+#include <cassert>
+#include <cstddef>
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <string>
 
-void CLIvisualizer::print() const {
+void CLIVisualizer::print(const LPUPool &pool) const {
 	std::cout << "---- Memory print out ----" << std::endl;
 	std::cout << "- full memory size " << std::to_string(memPtr->getMemorySize()) << std::endl;
 	std::cout << "\nLine num | instr hex | instr" << std::endl;
@@ -17,4 +23,60 @@ void CLIvisualizer::print() const {
 		std::printf("%s", str.c_str());
 		std::printf("\n");
 	}
+}
+
+void TXTFileVisualizer::print(const LPUPool &pool) const {
+	std::ofstream file;
+	file.open(path);
+	assert(file.is_open() && "Problem opening the file for printing");
+
+	auto records = pool.select<std::pair<const MemorySpace, const MemorySpace>>([](LPU* lpu)->std::pair<const MemorySpace, const MemorySpace>{
+				return lpu->getMemRecords();
+			});
+
+	std::vector<std::tuple<MemorySpace, LPUHandle, bool>> recordsList;
+	for (auto && r : records) {
+		recordsList.emplace_back(r.second.first, r.first, true);
+		if (!r.second.second.isEmpty()) {
+			recordsList.emplace_back(r.second.second, r.first, false);
+		}
+	}
+
+
+	uint64_t ridx = 0;
+	std::sort(recordsList.begin(), recordsList.end(), [](const auto &a, const auto &b){ return std::get<0>(a).start <= std::get<0>(b).start; }); // uff
+
+	file << "---- Memory print out ----" << std::endl;
+	file << "- full memory size " << std::to_string(memPtr->getMemorySize()) << std::endl;
+	file << "\nLine num | instr hex | instr" << std::endl;
+
+	for (uint64_t i = 0; i < memPtr->getMemorySize(); ++i) {
+		uint8_t instr = memPtr->fetch(i).value();
+		auto str = LPU::decode_tostring(instr); 
+		file << int(i) << " | ";
+		file << static_cast<int>(instr) << "|";
+		file << str.c_str();
+
+		MemorySpace ms = std::get<0>(recordsList[ridx]);
+		LPUHandle handle = std::get<1>(recordsList[ridx]);
+		bool active = std::get<2>(recordsList[ridx]);
+
+		if (ms.start == i) {
+			file << " -> MS START ";
+			file << "Handle: " << std::to_string(handle.id);
+			file << " Active " << std::to_string(active);
+		}
+		if (ms.start + ms.size-1 == i) {
+			file << " -> ~ MS END ~";
+			file << "Handle: " << std::to_string(handle.id);
+			file << " Active " << std::to_string(active);
+			ridx += 1;
+		}
+
+		file << std::endl;
+
+
+	}
+
+	file.close();
 }
--- src/visualizer.hpp
+++ src/visualizer.hpp
@@ -1,18 +1,27 @@
 #pragma once
 
+#include "lpu_pool.hpp"
 #include "memory.hpp"
 
 class VisualizerStrategy {
   public:
     VisualizerStrategy(BaseMemoryType *memPtr) { this->memPtr = memPtr; };
-	virtual void print() const = 0;
+	virtual void print(const LPUPool &pool) const = 0;
 
   protected:
     BaseMemoryType *memPtr;
 };
 
-class CLIvisualizer : public VisualizerStrategy {
+class CLIVisualizer : public VisualizerStrategy {
   public:
-    CLIvisualizer(BaseMemoryType *memPtr) : VisualizerStrategy(memPtr) {}
-	void print() const override;
+    CLIVisualizer(BaseMemoryType *memPtr) : VisualizerStrategy(memPtr) {}
+	void print(const LPUPool &pool) const override;
+};
+
+class TXTFileVisualizer: public VisualizerStrategy {
+  public:
+    TXTFileVisualizer(BaseMemoryType *memPtr, std::string path) : VisualizerStrategy(memPtr), path(path) {}
+	void print(const LPUPool &pool) const override;
+  private: 
+    const std::string path;
 };
